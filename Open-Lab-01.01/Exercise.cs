e2f5a7b550cf679335c411282d81990705bca148
MilosM33 <milosmarejka89@gmail.com>
2020-09-25 14:53:59 +0200

    IsLessThan10

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -4,9 +4,7 @@ namespace Open_Lab_01._01
 {
     public class Exercise
     {
-        public bool IsLessThan10(string input)
-        {
-            throw new NotImplementedException();
-        }
+        public bool IsLessThan10(string input) => int.Parse(input) < 10;
+        
     }
 }
